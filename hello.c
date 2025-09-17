d88afbc7c10a18730757db08f42844720cc0f1b7
fidel-stack <muendofidelis2@gmail.com>
2025-09-17 13:25:23 +0300

    Operation in c

--- hello.c
+++ hello.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main() {
-    //My first C program
-    printf("Hello, World!\n");
+    //My first C program 
+    printf("Hello, World! This is so much fun! I'mma start coding in C...!!\n");
+    printf("computer book");
+    printf("This is a computer book.\n");
     return 0;
 }
---
+++ variables.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+{
+	int age1=3, age2=4;
+	int sum;
+	sum= age1+age2;
+	printf("The sum is %d", sum);
+	return sum;
+}
