5dc6f5fdb1c056f403a7da713da6995e2195f628
fidel-stack <muendofidelis2@gmail.com>
2025-10-02 17:46:16 +0300

    Generating random numbers between 0 and 5 and getting a random fun fuct

---
+++ script.py
@@ -0,0 +1,17 @@
+import random 
+number = random.randint(0, 5) #To generate random numbers between 0 and 5
+#A random number between 0 and 5 is generated and a fun fact is shown as below 
+if number == 0:
+  print("Flamingos turn pink from eating shrimp")
+elif number == 1:
+  print("The only food that doesn\'t spoil is honey.")
+elif number == 2:
+  print("Shrimp can only swim backwards.")
+elif number == 3:
+  print("A taste bus\'s life span is about 10 days.")
+elif number == 4:
+  print("It is impossible to sneeze while sleeping")
+elif number == 5:
+  print("It is illigal to sing off-key in North Carolina")
+else:
+  print("Wrong input")          
