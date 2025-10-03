9e670d90b130d9fe1388c5d6c1a138abc0e4d411
fidel-stack <muendofidelis2@gmail.com>
2025-10-03 07:40:16 +0300

    if/elif/else statements and or logical operators

---
+++ seasons.py
@@ -0,0 +1,12 @@
+month = int(input("What month is this? "))
+
+if month == 1 or month == 2 or month == 3:
+  print("Winter season!")
+elif month == 4 or month == 5 or month == 6:
+  print("Spring season!")
+elif month == 7 or month == 8 or month == 9:
+  print("Summer season!")
+elif month == 10 or month == 11 or month == 12:
+  print("Autumn season!")  
+else:
+  print("Invalid")
