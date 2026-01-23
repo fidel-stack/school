a0a52d2671047c4978b57b06b6031cbab26d2630
fidel-stack <muendofidelis2@gmail.com>
2026-01-23 14:11:26 +0300

    Icon Toggler

---
+++ javaScript/Toggle/script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll(".favorite-icon").forEach(button =>{
+    button.addEventListener("click", () => {
+        if (button.classList.contains("filled")){
+            button.classList.remove("filled");
+            button.innerHTML = "&#9825;";
+        } else {
+            button.classList.add("filled");
+            button.innerHTML = "&#10084;";
+        }
+    });
+});
